71fa7e3e97d4088b9cb79a8099b7356778917e6d
Nyttep <pdubois@DESKTOP-7NOMVT5.localdomain>
2021-07-26 15:50:12 +0200

    added C09 C11 BSQ

--- BSQ/kayak.h
+++ BSQ/kayak.h
@@ -6,7 +6,7 @@
 /*   By: ascotto- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/20 11:29:38 by ascotto-          #+#    #+#             */
-/*   Updated: 2021/07/21 12:29:20 by ascotto-         ###   ########.fr       */
+/*   Updated: 2021/07/22 13:23:37 by ascotto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include <fcntl.h>
+# define BUF_SIZE 4096
 
 typedef struct s_answer
 {
@@ -25,19 +27,24 @@ typedef struct s_answer
 }	t_answer;
 
 char		**ft_split(char *str, char *charset);
-char		*ft_read_in_str(int letter_count, char *file_name);
-int			ft_line_count(int fd, unsigned int *letter_count);
-int			ft_line_count_open(char *file, unsigned int *letter_count);
-int			ft_check_str(char *str, int line_count);
+char		*ft_joinRobin(char *str1, char *str2);
+int			ft_main(int fd);
+char		*ft_filedup(int fd);
+int			ft_check_str(char *str);
+int			ft_lc(char *str);
+int			ft_line_count_strs(char **strs);
+int			ft_error(void);
 int			ft_check_tab_split(char **tab_split);
-int			**ft_init_tab(char **strs, int nbr_line, int char_per_line);
-void		ft_put_answer(char **strs, t_answer ans, int nbr_line);
-t_answer	ft_solve(int **tab, int line_count, int char_per_line);
+int			**ft_init_tab(char **strs);
+void		ft_put_answer(char **strs, t_answer ans);
+t_answer	ft_solve(int **tab, char **strs);
+void		ft_display(char **str);
 
-void	ft_putstr_error(char *str);
-void	ft_putstr(char	*str);
-void	ft_putchar(char c);
-int		ft_strlen(char *str);
-int		ft_atoi_count(char *str, int count);
-int		ft_len_number(int nbr);
+void		ft_putstr_error(char *str);
+int			ft_strcmp(char *s1, char *s2);
+void		ft_putstr(char	*str);
+void		ft_putchar(char c);
+int			ft_strlen(char *str);
+int			ft_atoi_count(char *str, int count);
+int			ft_len_number(int nbr);
 #endif 
--- BSQ/main.c
+++ BSQ/main.c
@@ -6,7 +6,8 @@
 /*   By: ascotto- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/20 11:03:59 by ascotto-          #+#    #+#             */
-/*   Updated: 2021/07/21 12:41:34 by ascotto-         ###   ########.fr       */
+/*   Updated: 2021/07/22 13:20:29 by ascotto-         ###   ########.fr       */
+/*   Updated: 2021/07/22 10:18:58 by ascotto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +16,52 @@
 
 int	main(int argc, char **argv)
 {
-	char				*str;
-	t_answer			solution;
-	char				**tab_split;
-	int					**tab_int;
-	unsigned int		letter_count;
-	unsigned int		line_count;
+	int					fd;
+	int					i;
 
-	(void)argc;
-	line_count = ft_line_count_open(argv[1], &letter_count);
-	str = ft_read_in_str(letter_count, argv[1]);
-	tab_split = ft_split(str, "\n");
-	tab_int = ft_init_tab(tab_split, line_count - 1, ft_strlen(tab_split[1]));
-	solution = ft_solve(tab_int, line_count - 1, ft_strlen(tab_split[1]));
-	ft_put_answer(tab_split, solution, line_count - 1);
-	int i = 0;
-	while (tab_split[i])
+	if (argc == 1)
 	{
-		printf("%s\n", tab_split[i]);
-		i++;
+		if (ft_main(0) == 0)
+			return (ft_error());
+		return (0);
 	}
-	return (0);
+	i = 1;
+	while (i < argc)
+	{
+		printf("Testing %s\n", argv[i]);
+		fd = open(argv[i], O_RDONLY);
+		if (fd == -1)
+			return (ft_error());
+		if (ft_main(fd) == 0)
+			ft_error();
+		i++;
+		if (i < argc)
+			write(1, "\n", 1);
+	}
+}
+
+int	ft_main(int fd)
+{
+	char	*str;
+	char	**tab_split;
+	int		**tab_int;
+	int		i;
+
+	i = -1;
+	str = ft_filedup(fd);
+	if (!str)
+		return (0);
+	if (ft_check_str(str) == -1)
+		return (0);
+	tab_split = ft_split(str, "\n");
+	if (ft_check_tab_split(tab_split) == -1)
+		return (0);
+	tab_int = ft_init_tab(tab_split);
+	ft_put_answer(tab_split, ft_solve(tab_int, tab_split));
+	ft_display(tab_split);
+	while (++i < ft_lc(str))
+		free(tab_int[i]);
+	free(str);
+	free(tab_int);
+	return (1);
 }
